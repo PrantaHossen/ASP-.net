15fcb6cb5e71faa154cd9795d9f034417f7db423
PrantaHossen <prantahossen@gmail.com>
2024-07-06 12:34:07 +0600

    Default Routing

---
+++ Final Term(Web API)/DefaultRoutings/DefaultRoutings/Controllers/DepartmentController.cs
@@ -0,0 +1,54 @@
+﻿using DefaultRoutings.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DefaultRoutings.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public List<DepartmentModel> Get()
+        {
+            List<DepartmentModel> dept = new List<DepartmentModel>();
+
+            for (int i = 0; i < 10; i++) {
+                dept.Add(new DepartmentModel()
+                { 
+                    Dept_Id = i+1,
+                    Dept_Name= "Department Name " +i
+                    
+                });
+            }
+            return dept;
+        }
+
+        public string Post(DepartmentModel d)
+        {
+            List<DepartmentModel> dept = new List<DepartmentModel>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                dept.Add(new DepartmentModel()
+                {
+                    Dept_Id = i + 1,
+                    Dept_Name = "Department Name " + i
+
+                });
+            }
+            dept.Add(d);
+            return "New Department Added SuccessFully";
+        }
+
+        public string Put()
+        {
+            return "This is from Put Method";
+        }
+        public string Delete()
+        {
+            return "This is from Delete Method";
+        }
+    }
+}
---
+++ Final Term(Web API)/DefaultRoutings/DefaultRoutings/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DefaultRoutings.WebApiApplication" Language="C#" %>
