bbaa57e1c9338cfd0b966d8b069e15fcdb27d872
PrantaHossen <prantahossen@gmail.com>
2024-07-06 15:00:56 +0600

    UMS Routing(student)

---
+++ Final Term(Web API)/UMSRouting/UMSRouting/Models/ViewModel/DepartmentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UMSRouting.Models.ViewModel
+{
+    public class DepartmentModel
+    {
+        public int Department_Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Final Term(Web API)/UMSRouting/UMSRouting/Models/ViewModel/StudentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UMSRouting.Models.ViewModel
+{
+    public class StudentModel
+    {
+        public int Student_Id { get; set; }
+        public string Name { get; set; }
+        public string Dob { get; set; }
+        public string Gender { get; set; }
+        public string Cgpa { get; set; }
+        public int Department_Id { get; set; }
+
+        public virtual DepartmentModel Department { get; set; }
+    }
+}
